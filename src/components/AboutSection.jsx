39fbcb3b944adb017a0b4eb2dbbd1b1be96b2adb
Rizky Albani <rzkyalbani@gmail.com>
2025-08-02 20:48:59 +0700

    feat(ui): Create about section ui responsive to tablet view

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,14 +1,25 @@
+import aboutImg from "../assets/images/about-me.png";
+
 function AboutSection() {
     return (
-        <section className="flex flex-col items-center gap-6 px-8 pt-7 pb-12 w-full h-auto">
-            <div className="flex flex-col gap-2 items-center">
-                <h3 className="text-zinc-950 text-4xl font-bold">About Me</h3>
-                <div className="w-24 h-1 bg-zinc-950 rounded-sm"></div>
-                <p className="text-base font-medium text-zinc-500">A web dev and ui/ux design enthusiast.</p>
+        <section className="flex h-auto w-full flex-col items-center gap-6 px-8 pt-7 pb-12 md:p-16 md:pt-12">
+            <div className="flex flex-col items-center gap-2 md:w-full md:items-start md:gap-3">
+                <h3 className="text-4xl font-bold text-zinc-950 md:text-5xl">
+                    About Me
+                </h3>
+                <div className="h-1 w-24 rounded-sm bg-zinc-950 md:h-2 md:w-32 md:rounded-xs"></div>
+                <p className="text-base font-medium text-zinc-500 md:text-lg">
+                    A web dev and ui/ux design enthusiast.
+                </p>
             </div>
-            <div>
-                <div className="flex flex-col gap-6">
-                    <p className="text-sm text-zinc-700">
+            <div className="flex flex-col gap-6">
+                <img
+                    src={aboutImg}
+                    alt="About Me"
+                    className="hidden max-w-1/2 md:block"
+                />
+                <div className="flex flex-col gap-6 md:max-w-4/5 md:gap-2">
+                    <p className="text-sm text-zinc-700 md:text-base">
                         I enjoy exploring the JavaScript ecosystem — from
                         building interfaces with React, styling with Tailwind,
                         to understanding how everything fits together. I care
@@ -17,7 +28,7 @@ function AboutSection() {
                         learning design tools like Figma to understand both the
                         dev and design sides.
                     </p>
-                    <p className="text-sm text-zinc-700">
+                    <p className="text-sm text-zinc-700 md:text-base">
                         For me, learning never stops. Whether I am building a
                         component, fixing a layout bug, or experimenting with a
                         new animation library, I always try to enjoy the
