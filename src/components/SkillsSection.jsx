3c9f9dc8c51ebef61cf60a318b56db27a3c1367f
Rizky Albani <rzkyalbani@gmail.com>
2025-08-02 20:53:04 +0700

    feat(ui): Create tech stack section responsive to tablet view

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -2,21 +2,23 @@ import icons from "../helpers/icons";
 
 function SkillsSection() {
     return (
-        <section className="flex flex-col gap-9 px-8 py-16 bg-zinc-950">
-            <div className="flex flex-col gap-2 items-center">
-                <h3 className="text-white text-4xl font-bold">Tech Stack</h3>
-                <div className="w-24 h-1 bg-white rounded-sm"></div>
-                <p className="text-base font-medium text-zinc-200">
+        <section className="flex flex-col gap-9 bg-zinc-950 px-8 py-16">
+            <div className="flex flex-col items-center gap-2">
+                <h3 className="text-4xl font-bold text-white md:text-5xl">
+                    Tech Stack
+                </h3>
+                <div className="h-1 w-24 rounded-sm bg-white md:h-2 md:w-32 md:rounded-xs"></div>
+                <p className="text-base font-medium text-zinc-200 md:text-lg">
                     Technologies I’ve touched so far.
                 </p>
             </div>
-            <div className="flex justify-center items-center flex-wrap py-2 gap-5">
+            <div className="flex flex-wrap items-center justify-center gap-5 py-2">
                 {icons.map((i) => (
                     <div
                         key={i.id}
-                        className="flex flex-col justify-center items-center gap-2 w-24 h-24 border-2 border-zinc-300 rounded-4xl rounded-tl-none"
+                        className="flex h-24 w-24 flex-col items-center justify-center gap-2 rounded-4xl rounded-tl-none border-2 border-zinc-300"
                     >
-                        <img src={i.src} alt={i.name} className="w-11 h-11" />
+                        <img src={i.src} alt={i.name} className="h-11 w-11" />
                         <p className="text-xs font-bold text-white">{i.name}</p>
                     </div>
                 ))}
