2fec793891edb38ad9b95ad62c28ec090fcb38ed
Rizky Albani <rzkyalbani@gmail.com>
2025-08-02 17:36:06 +0700

    style(ui): Create navbar style for tablet view

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,9 +1,6 @@
 function Hamburger({ isOpen, toggle }) {
     return (
-        <button
-            className="flex flex-col gap-1"
-            onClick={toggle}
-        >
+        <button className="flex flex-col gap-1" onClick={toggle}>
             <span
                 className={`w-6 h-1 bg-zinc-950 rounded-full transition-all duration-300 ${
                     isOpen ? "-rotate-45 translate-y-2" : ""
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,44 @@
 import { useState } from "react";
 import Hamburger from "./Hamburger";
+import arrrowDownIcon from "../assets/icons/arrow-down-icon.svg";
 
 function Navbar({ logo }) {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="px-8 w-full h-14 flex items-center justify-between bg-white shadow-sm">
-            <div className="w-auto h-7">
+        <nav className="px-8 md:px-16 w-full h-14 md:h-24 flex items-center justify-between bg-white shadow-sm">
+            <div className="w-auto h-7 md:h-11">
                 <img src={logo} alt="Logo" className="w-full h-full" />
             </div>
-            <div>
+            <div className="md:hidden">
                 <Hamburger isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
             </div>
+
+            {/* Desktop link */}
+            <div className="hidden md:flex md:gap-7 md:items-center">
+                <a href="" className="text-base font-medium text-zinc-950">
+                    Home
+                </a>
+                <a href="" className="text-base font-medium text-zinc-950">
+                    About
+                </a>
+                <a href="" className="text-base font-medium text-zinc-950">
+                    Projects
+                </a>
+                <a href="" className="text-base font-medium text-zinc-950">
+                    Contact
+                </a>
+            </div>
+
+            {/* Desktop CTA Button */}
+            <button className="hidden md:flex md:gap-1 md:px-5 items-center h-10 bg-zinc-950 rounded-lg">
+                <span className="text-white text-base font-medium">Resume</span>
+                <img
+                    src={arrrowDownIcon}
+                    alt="Download Icon"
+                    className="h-5 w-5"
+                />
+            </button>
 
             {/* Mobile menu */}
             <div
